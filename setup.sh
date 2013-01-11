bf3db55393f055c433c93e1f22e522c7aabed650
Johannes Edelstam <johannes.edelstam@gmail.com>
2013-01-11 16:23:46 +0100

    Add global gitignore

--- setup.sh
+++ setup.sh
@@ -3,7 +3,11 @@ defaults write NSGlobalDomain KeyRepeat -int 0
 
 defaults write org.vim.MacVim MMNativeFullScreen 0
 
+
 pwd=$PWD
+
+git config --global core.excludesfile $pwd/gitignore_global
+
 cd ~
 ln -s $pwd/vim/vimrc.before .vimrc.before
 ln -s $pwd/vim/vimrc.after .vimrc.after
