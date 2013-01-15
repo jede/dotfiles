246b61eea5d3e4d3aba41a3fcdfb91bd124b6bc7
Johannes Edelstam <johannes.edelstam@gmail.com>
2013-01-15 11:07:41 +0100

    Added ackrc

--- setup.sh
+++ setup.sh
@@ -11,9 +11,10 @@ git config --global core.excludesfile $pwd/gitignore_global
 cd ~
 ln -s $pwd/vim/vimrc.before .vimrc.before
 ln -s $pwd/vim/vimrc.after .vimrc.after
-ln -s $pwd/vim/janus .janus
+[[ !(-a .janus) ]] && ln -s $pwd/vim/janus .janus
 ln -s $pwd/zshrc .zshrc
 ln -s $pwd/gemrc .gemrc
+ln -s $pwd/ackrc .ackrc
 cd $pwd
 
 
